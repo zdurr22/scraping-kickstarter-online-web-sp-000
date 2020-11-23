18525562c9ea399d4c56d7278ff5c2bb15163aa0
Zachary Durr <zdurr22@gmail.com>
2020-11-23 01:01:55 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,7 +10,9 @@ def create_project_hash
 
   kickstarter.css("li.project.grid_4").each do |project|
     projects[project] = {}
-    
+  end
+
+  projects
 
 end
 # projects: kickstarter.css("li.project.grid_4")
