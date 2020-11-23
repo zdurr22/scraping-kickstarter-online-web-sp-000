bb21432038766d1c808d346b9d3dc04b236cc294
Zachary Durr <zdurr22@gmail.com>
2020-11-23 01:01:39 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,10 @@ def create_project_hash
 
   projects = {}
 
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+    
+
 end
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
