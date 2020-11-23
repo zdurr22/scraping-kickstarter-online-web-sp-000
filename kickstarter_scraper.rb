ef3d4976bae7b7e2df41c5de2324632a74bace70
Zachary Durr <zdurr22@gmail.com>
2020-11-23 00:48:46 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,5 +7,6 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   binding.pry
 end
-
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
 create_project_hash
