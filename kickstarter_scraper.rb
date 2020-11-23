859c2d2df61e67225344c9ca5a9a5adebf202715
Zachary Durr <zdurr22@gmail.com>
2020-11-23 00:56:28 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,4 +10,5 @@ end
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
 create_project_hash
