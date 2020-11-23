7b9ab6650cd3959bf5b54c6ec7d0de604a025fc0
Zachary Durr <zdurr22@gmail.com>
2020-11-23 01:05:09 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,7 +13,7 @@ def create_project_hash
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
-      :location => project.css("ul.project-meta").text.strip
+      :location => project.css("ul.project-meta span.location-name").text.strip
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
     }
   end
